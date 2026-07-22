4cd27e28f4f0a0f530e952dd598d56c94f7ab43e
sayani-code <sayanisaha0806@gmail.com>
2026-07-22 22:24:54 +0530

    feat: add currency converter using functions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Currency Converter
 
 rates = {
     "USD": 1.00,
@@ -7,17 +6,33 @@ rates = {
     "GBP": 0.75
 }
 
-amount = float(input("Enter amount: "))
-from_currency = input("From Currency (USD/INR/EUR/GBP): ").upper()
-to_currency = input("To Currency (USD/INR/EUR/GBP): ").upper()
+def get_amount():
+    amount = float(input("Enter amount: "))
+    return amount
+
+
+def get_currency(currency_ip):
+    currency = input(currency_ip).upper()
+    return currency
+
+
+def convert_currency(amount, from_currency, to_currency):
+    usd = amount / rates[from_currency]  #amount->USD
+    converted = usd * rates[to_currency] #USD->to_currency
+    return converted
+
+
+def display_result(amount, from_currency, to_currency, converted):
+    print(f"\n{amount:.2f} {from_currency} = {converted:.2f} {to_currency}")
+
+
+# Main Program
+amount = get_amount()
+from_currency = get_currency("From Currency (USD/INR/EUR/GBP): ")
+to_currency = get_currency("To Currency (USD/INR/EUR/GBP): ")
 
 if from_currency in rates and to_currency in rates:
-    # Convert to USD first
-    usd = amount / rates[from_currency]
-
-    # Convert USD to target currency
-    converted = usd * rates[to_currency]
-
-    print(f"\n{amount} {from_currency} = {converted:.2f} {to_currency}")
+    result = convert_currency(amount, from_currency, to_currency)
+    display_result(amount, from_currency, to_currency, result)
 else:
     print("Invalid currency code!")
